6946d5d587a856d634192a06691ba666f4b19337
Johndale Alfred Julian <johndale@binary.digital>
2018-10-31 16:28:32 +0800

    Added some javadocs and ViewUtils

--- src/main/java/io/fueledbysushi/commonkotlinextensions/ContextUtils.kt
+++ src/main/java/io/fueledbysushi/commonkotlinextensions/ContextUtils.kt
@@ -2,6 +2,8 @@ package io.fueledbysushi.commonkotlinextensions
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
@@ -9,14 +11,24 @@ import android.widget.Toast
 /**
  * Created by Johndale Alfred Julian on 10/31/2018. All Rights Reserved.
  */
-fun Context.showToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+/**
+ * Shows a short @see Toast
+ */
+fun Context.showToast(text: String): Toast {
+    return Toast.makeText(this, text, Toast.LENGTH_SHORT).also { it.show() }
 }
 
-fun Context.showToastLong(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+/**
+ * Shows a long @see Toast
+ */
+fun Context.showToastLong(text: String): Toast {
+    return Toast.makeText(this, text, Toast.LENGTH_LONG).also { it.show() }
 }
 
+/**
+ * Hides soft keyboard
+ */
 fun Activity.hideKeyboard() {
     val view = this.currentFocus
     val methodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -24,6 +36,9 @@ fun Activity.hideKeyboard() {
     methodManager.hideSoftInputFromWindow(view!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
 }
 
+/**
+ * Shows soft keyboard
+ */
 fun Activity.showKeyboard() {
     val view = this.currentFocus
     val methodManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -31,14 +46,56 @@ fun Activity.showKeyboard() {
     methodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
 }
 
+/**
+ * Logs error with the context name as its TAG
+ */
 fun Context.logError(message: String) {
     Log.e(this::class.java.simpleName, message)
 }
 
+/**
+ * Logs info with the context name as its TAG
+ */
 fun Context.logInfo(message: String) {
     Log.i(this::class.java.simpleName, message)
 }
 
-fun Activity.logWarning(message: String) {
+/**
+ * Logs warning with the Context name as its TAG
+ */
+fun Context.logWarning(message: String) {
     Log.w(this::class.java.simpleName, message)
 }
+
+/**
+ * Returns the complete device and app details
+ */
+fun getDeviceDetails(): String {
+    return StringBuilder().apply {
+        append("OS = " + System.getProperty("os.version") + "; ")
+        append("SDK_INT = " + Build.VERSION.SDK_INT + "; ")
+        append("BUILD_DEVICE = " + Build.DEVICE + "; ")
+        append("BUILD_RELEASE = Android " + Build.VERSION.RELEASE + "; ")
+        append("BUILD_MODEL = " + Build.MODEL + "; ")
+        append("APPLICATION_ID = " + BuildConfig.APPLICATION_ID + "; ")
+        append("BUILD_MANUFACTURER = " + Build.MANUFACTURER + "; ")
+        append("Version Name = " + BuildConfig.VERSION_NAME)
+    }.toString()
+}
+
+/**
+ * Returns the screen density
+ * Example: ldpi, mdpi, ...
+ */
+fun Context.getScreenDensity(): String {
+    return when (resources.displayMetrics.densityDpi) {
+        DisplayMetrics.DENSITY_LOW -> "ldpi"
+        DisplayMetrics.DENSITY_MEDIUM -> "mdpi"
+        DisplayMetrics.DENSITY_HIGH -> "hdpi"
+        DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_280 -> "xhdpi"
+        DisplayMetrics.DENSITY_XXHIGH, DisplayMetrics.DENSITY_360, DisplayMetrics.DENSITY_400, DisplayMetrics.DENSITY_420 -> "xxhdpi"
+        DisplayMetrics.DENSITY_XXXHIGH, DisplayMetrics.DENSITY_560 -> "xxxhdpi"
+        else -> "Unknown code ${resources.displayMetrics.densityDpi}"
+    }
+}
+
--- src/main/java/io/fueledbysushi/commonkotlinextensions/ViewUtils.kt
+++ src/main/java/io/fueledbysushi/commonkotlinextensions/ViewUtils.kt
@@ -1,5 +1,54 @@
 package io.fueledbysushi.commonkotlinextensions
 
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import java.util.*
+
 /**
  * Created by Johndale Alfred Julian on 10/31/2018. All Rights Reserved.
  */
+
+/**
+ * Returns all Views of Type <T> and performs an action on each view
+ */
+inline fun <reified T : View> ViewGroup.allViewsOfType(action: (T) -> Unit) {
+    val views = Stack<View>()
+
+    views.addAll((0 until childCount).map(this::getChildAt))
+
+    while (!views.isEmpty()) {
+        views.pop().let {
+            if (it is T) action(it)
+            if (it is ViewGroup) {
+                views.addAll((0 until childCount).map(this::getChildAt))
+            }
+        }
+    }
+}
+
+/**
+ * Animate a @see View using fade-in animation
+ */
+fun View.fadeIn() {
+    startAnimation(AnimationUtils.loadAnimation(context, android.R.anim.fade_in))
+    visibility = View.VISIBLE
+}
+
+/**
+ * Animate a @see View using fade-out animation but
+ * the view still takes pixel space
+ */
+fun View.fadeOut() {
+    startAnimation(AnimationUtils.loadAnimation(context, android.R.anim.fade_out))
+    visibility = View.INVISIBLE
+}
+
+/**
+ * Animate a @see View using fade-out animation,
+ * the @see View won't take any pixel space
+ */
+fun View.fadeOutGone() {
+    startAnimation(AnimationUtils.loadAnimation(context, android.R.anim.fade_out))
+    visibility = View.GONE
+}
